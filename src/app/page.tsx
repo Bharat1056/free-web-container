f91b0a4edf63fdbfad2369609e1b11bbda53d2ac
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-09-06 12:21:35 +0530

    feat: update trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { caller } from "@/trpc/server";
+
+export default async function Page() {
+  const data = await caller.hello({ text: "Antonio server" });
+  console.log(data.greeting);
   return <div className="font-bold">Hello World</div>;
 }
