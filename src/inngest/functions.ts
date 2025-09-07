7029b0ba37a81e80295b9210a40e1c3e4011b43e
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-09-07 18:59:06 +0530

    feat: integrate E2B code interpreter and add Next.js sandbox template with Docker support

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,22 +1,35 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "@/inngest/utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-three");
+      return sandbox.sandboxId;
+    });
     // create an agent
     const writer = createAgent({
       name: "writer",
       system:
-        "You are an expert writer.  You write readable, concise, simple content.",
+        "You are an expert next.js developer.  You write readable, maintainable code. You write simple next js and React templates",
       model: gemini({ model: "gemini-1.5-flash" }),
     });
-    console.log("Writing a blog post about ", event.data.value);
     const { output } = await writer.run(
-      `Write a blog post about ${event.data.value}`
+      `Write the following snippet of code in next.js and React: ${event.data.value}`
     );
+
     console.log(output);
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
