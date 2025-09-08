2a2a8a60037edd32012c7d69ae62a8f18baf57dc
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-09-08 23:46:13 +0530

    feat: downgrade zod to version 3.23.0, add new tools for terminal and file operations in Inngest functions, and introduce a prompt for the coding agent.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,9 @@
 import { Sandbox } from "@e2b/code-interpreter";
-import { gemini, createAgent } from "@inngest/agent-kit";
+import { gemini, createAgent, createTool } from "@inngest/agent-kit";
 import { inngest } from "./client";
 import { getSandbox } from "@/inngest/utils";
+import { z } from "zod";
+import { PROMPT } from "@/prompt";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -13,10 +15,107 @@ export const helloWorld = inngest.createFunction(
     });
     // create an agent
     const writer = createAgent({
-      name: "writer",
-      system:
-        "You are an expert next.js developer.  You write readable, maintainable code. You write simple next js and React templates",
-      model: gemini({ model: "gemini-1.5-flash" }),
+      name: "code-agent",
+      description:
+        "An Exeprt coding agent that writes code in next.js, tailwind CSS, shadcn",
+      system: PROMPT,
+      model: gemini({
+        model: "gemini-1.5-flash",
+        defaultParameters: {},
+      }),
+      tools: [
+        // terminal tool
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffer = { stdout: "", stderr: "" };
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffer.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffer.stderr += data;
+                  },
+                });
+                return result.stdout;
+              } catch (e) {
+                console.error(
+                  `Command failed: ${e}\nstdout: ${buffer.stdout}\nstderr: ${buffer.stderr}`
+                );
+                return `Command failed: ${e}\nstdout: ${buffer.stdout}\nstderr: ${buffer.stderr}`;
+              }
+            });
+          },
+        }),
+        // createOrUpdateFiles tool
+        createTool({
+          name: "createOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            /* this is the output format*/
+            files: z.array(
+              z.object({
+                path: z.string(),
+                content: z.string(),
+              })
+            ),
+          }),
+          handler: async ({ files }, { step, network }) => {
+            const newFiles = await step?.run(
+              "createOrUpdateFiles",
+              async () => {
+                try {
+                  const updateFiles = network.state.data.files || {};
+                  const sandbox = await getSandbox(sandboxId);
+                  for (const file of files) {
+                    await sandbox.files.write(file.path, file.content);
+                    updateFiles[file.path] = file.content;
+                  }
+                  return updateFiles;
+                } catch (error) {
+                  return "Error: " + error;
+                }
+              }
+            );
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
+            }
+          },
+        }),
+        // readFile tool
+        createTool({
+          name: "readFile",
+          description: "Read a file from the sandbox",
+          parameters: z.object({
+            files: z.array(z.string()),
+          }),
+          handler: async ({ files }, { step }) => {
+            return await step?.run("readFiles", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const contents = [];
+                for (const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({ path: file, content });
+                }
+                return JSON.stringify(contents);
+              } catch (error) {
+                return "Error: " + error;
+              }
+            });
+          },
+        }),
+      ],
+      lifecycle: {
+        onResponse: 
+      }
     });
     const { output } = await writer.run(
       `Write the following snippet of code in next.js and React: ${event.data.value}`
