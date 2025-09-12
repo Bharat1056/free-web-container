297a4d0d63b9c80ef94c356cb435f8b05c6a31d4
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-09-12 23:47:32 +0530

    feat: update project and message handling with TRPC integration; add message form and card components, implement project view with message display

---
+++ src/modules/projects/ui/views/project-view.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Suspense } from "react";
+import { MessagesContainer } from "@/modules/projects/ui/components/messages-container";
+
+interface Props {
+  projectId: string;
+}
+
+export const ProjectView = ({ projectId }: Props) => {
+  const trpc = useTRPC();
+  const { data: project } = useSuspenseQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    })
+  );
+
+  return (
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          <Suspense fallback={<>Loading Messages...</>}>
+            <MessagesContainer projectId={projectId} />
+          </Suspense>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+          TODO: Preview
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
